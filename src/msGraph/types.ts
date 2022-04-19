98b026e196f97810d3820a97b6ceb5675dcc7ee6
Gang Zhang <gzhangx@hotmail.com>
2022-04-19 10:09:52 -0400

    added types

---
+++ src/msGraph/types.ts
@@ -0,0 +1,9 @@
+import { IMsGraphCreds, IAuthOpt, ILogger, IDriveItemInfo, IMsGraphConn,IMsGraphOps, ITokenInfo } from './msauth';
+import { IMsGraphDirPrms, IDirCreateResponse, IFileSearchResult } from './msdir';
+import { IMsGraphExcelItemOpt,IMsExcelOps, IReadSheetValues } from './msExcell';
+
+export {
+    IMsGraphCreds,IAuthOpt,ILogger,IDriveItemInfo,ITokenInfo,
+    IMsGraphDirPrms,IDirCreateResponse,IFileSearchResult,
+    IMsGraphExcelItemOpt,IMsExcelOps,IReadSheetValues,
+}
