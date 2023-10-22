e87e517af65ad6b4eaa65fc43b1b578b9cdd9dd4
Gang Zhang <gzhangx@hotmail.com>
2023-10-22 13:04:43 -0400

    added google drive

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,15 @@ import * as util from './util'
 import { getClient, getClientByEnv, getTokenFromCode, getClientCredsByEnv } from './googleApi'
 import * as gsAccount from './google/googleApiServiceAccount'
 
+import * as googleDrive from './google/drive';
 //google should not be used
-const google = {
+const googleOld = {
     getClient, getClientByEnv,getTokenFromCode,getClientCredsByEnv
 }
 
 import * as msauth from './msGraph/msauth'
 import * as msdir from './msGraph/msdir'
 import * as msExcell from './msGraph/msExcell'
-import * as httpRequest from './httpRequest'
 
 const msGraph = {
     msauth,
@@ -19,10 +19,15 @@ const msGraph = {
     msExcell
 }
 
+const google = {
+    drive: googleDrive,
+    gsAccount,
+}
 export {
-    google, //don't use
+    googleOld, //don't use
     msGraph,
     gsAccount,
     //httpRequest, moved to util
     util,
+    google,
 }
