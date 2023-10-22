c5757323a4ec5d50a3f79571729092a1d6fc8e98
Gang Zhang <gzhangx@hotmail.com>
2023-10-22 12:37:02 -0400

    add drive

---
+++ src/google/drive.ts
@@ -0,0 +1,33 @@
+import * as util from '../util'
+
+const rootUrl = 'https://www.googleapis.com/drive/v3';
+
+export interface AddPermissionData {
+    role: 'writer' | 'reader';
+    type: 'user';
+    emailAddress: string;
+}
+
+export interface AddPermissionRequest {
+    emailMessage?: string;
+    sendNotificationEmail?: boolean;
+    fileId: string;
+    data: AddPermissionData;
+}
+function addPermission(token: string, req: AddPermissionRequest) {
+    return util.doHttpRequest({
+        method: 'POST',
+        url: `${rootUrl}/files/${req.fileId}/permissions?emailMessage=${req.emailMessage}&sendNotificationEmail=${req.sendNotificationEmail || false}`,
+        data: req.data,
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`,
+        }
+    })
+}
+
+export function getGoogleDriveOps(token: string) {
+    return {
+        addPermission: req => addPermission(token, req),
+    }
+}
